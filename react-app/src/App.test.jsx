d0dcf55141a4e9e5ed8c6fefc10e40e363e47340
Rachel <hurui.lcy@gmail.com>
2024-11-13 23:03:09 -0700

    test

--- react-app/src/App.test.jsx
+++ react-app/src/App.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import BookingForm from "./BookingForm";
 
 test('Renders the BookingForm heading', ()=>{
@@ -7,3 +7,15 @@ test('Renders the BookingForm heading', ()=>{
   expect(headingElement).toBeInTheDocument();
 
 })
+
+
+test('guests input field has a minimum value of 1',() =>{
+  render(<BookingForm />);
+  const guestsInput =  screen.getByLabelText(/guests/);
+  fireEvent.change(guestsInput, {target : {value : "3"}});
+
+  expect(guestsInput).toHaveAttribute('min','1');
+})
+
+
+test('')
