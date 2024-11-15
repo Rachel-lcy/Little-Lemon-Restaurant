e0589904cf0800da8e1968fe09c143c62f4616af
Rachel <hurui.lcy@gmail.com>
2024-11-15 13:53:26 -0700

    update

--- react-app/src/App.test.jsx
+++ react-app/src/App.test.jsx
@@ -3,19 +3,23 @@ import BookingForm from "./BookingForm";
 
 test('Renders the BookingForm heading', ()=>{
   render(<BookingForm />);
-  const headingElement = screen.getByText("Little Lemon Restaurant Reservation");
-  expect(headingElement).toBeInTheDocument();
+  const handleSubmit = screen.getByText("Little Lemon Restaurant Reservation");
+  expect(handleSubmit).toBeInTheDocument();
 
 })
 
 
 test('guests input field has a minimum value of 1',() =>{
   render(<BookingForm />);
-  const guestsInput =  screen.getByLabelText(/guests/);
+  const guestsInput =  screen.getByLabelText(/guests/i);
   fireEvent.change(guestsInput, {target : {value : "3"}});
 
   expect(guestsInput).toHaveAttribute('min','1');
-})
+});
 
 
-test('')
+test('name input field is required', () => {
+  render(<booking />);
+  const nameInput = screen.getByAltText(/name/i);
+  expect(nameInput).toBeRequired();
+});
